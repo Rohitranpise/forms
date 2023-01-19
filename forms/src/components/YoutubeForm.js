bab58182b0dd593b820ea45b9ebf3ae6874f0ba4
Rohitranpise <rohitranpise2758@gmail.com>
2023-01-19 18:01:38 +0530

    first comm

---
+++ forms/src/components/YoutubeForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useFormik } from "formik";
+function YoutubeForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <label htmlFor="email">email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <label htmlFor="channel">channel</label>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
+
+        <button>submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default YoutubeForm;
